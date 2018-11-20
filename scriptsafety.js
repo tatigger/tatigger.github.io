4f69e2470a519041b5964e9e78878e2dedefce95
Terri Lyman <tltig@aol.com>
2018-11-19 20:01:00 -0700

    Ch10 again

--- scriptsafety.js
+++ scriptsafety.js
@@ -7,6 +7,13 @@
 
     Filename: scriptsafety.js
     
+    Google Maps doesn't seem to load prior to all the functions running, 
+    leaving an error in the java console "ReferenceError: Can't find variable:
+    google". I researched the problem, and the documents I found said that the 
+    API is not loading prior to function call. I added a longer timer and separated
+    the function out to try to allow time for the API to load, but it does not seem
+    to work anyway.  I can't further troubleshoot because I can't get past that
+    error.    
 */
 
 "use strict";
@@ -15,6 +22,7 @@
 var waitForUser; // for timeout if no response to request for location data
 // area to print location or error message
 var directDiv = document.getElementById("directions");
+// area to print altitude or error message
 var altDiv = document.getElementById("currAltitude");
 
 // get coordinates and print in geolocations area
@@ -40,25 +48,33 @@ function createDirections(position) {
       directDiv.innerHTML = "Current Location is Latitude " 
       + currPosLat + ", Longitude " + currPosLng;
       
-} 
-
-function createMap(position) {
- //    var currPosLat = position.coords.latitude;
- //    var currPosLng = position.coords.longitude;
-     var mapOptions = {
+      var mapOptions = {
         center: new google.maps.LatLng(currPosLat, currPosLng),
         zoom: 12
         };
       var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
+      
+} 
+// separate function to try to allow google map to load prior to function run
+//function createMap(position) {
+//     var currPosLat = position.coords.latitude;
+//     var currPosLng = position.coords.longitude;
+//     var mapOptions = {
+//        center: new google.maps.LatLng(currPosLat, currPosLng),
+//        zoom: 12
+//        };
+//      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//}
 
 function geoTest() { // check if location data allowed and available
     waitForUser = setTimeout(fail, 10000);
     if (navigator.geolocation) { 
         navigator.geolocation.getCurrentPosition(createDirections, fail, {timeout: 100000});
-        var waitForGoogleLoad = setTimeout(createMap, 30000)
+        // timer to allow google to load on safety page, will error "google not found" if 
+        // api doesn't load before the function runs
+//        var waitForGoogleLoad = setTimeout(createMap, 30000);
     } else {
-        fail();
+        fail(); // error for long, lat and alt
     }
 }
 
